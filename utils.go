00f219d84e77fda04c3b2d75a022f1b7d6dc4439
unknown <ihellostereo@gmail.com>
2025-06-19 20:47:14 +0700

    conditional isNumber

--- utils.go
+++ utils.go
@@ -17,3 +17,12 @@ func isString(val interface{}) bool {
 	_, ok := val.(string)
 	return isNotEmpty(val) && ok
 }
+
+func isNumber(val interface{}) bool {
+	switch val.(type) {
+		case int, int8, int16, int32, int64, float32, float64:
+			return isNotEmpty(val) && true
+		default:
+			return false
+	}
+}
