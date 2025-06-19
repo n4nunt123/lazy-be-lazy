160ce1c32ab1f77ca9cb75d66f584deab31f5ec9
unknown <ihellostereo@gmail.com>
2025-06-19 20:46:57 +0700

    conditional isString

--- utils.go
+++ utils.go
@@ -12,3 +12,8 @@ func isEmpty(val interface{}) bool {
 func isNotEmpty(val interface{}) bool {
 	return !isMissing(val)
 }
+
+func isString(val interface{}) bool {
+	_, ok := val.(string)
+	return isNotEmpty(val) && ok
+}
